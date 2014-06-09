40912ff09844823a8ab37c44808278169b5495a2
Andy Wang <climber2002@gmail.com>
2014-06-09 20:19:30 +0930

    finish ui architecture

---
+++ app/controllers/provinces_controller.rb
@@ -0,0 +1,14 @@
+class ProvincesController < ApplicationController
+
+  def index
+      @provinces = Province.all
+
+      respond_to do |format|
+        format.json do  
+          feature_collection = Province.to_feature_collection @provinces
+          render json: RGeo::GeoJSON.encode(feature_collection)
+        end
+      end
+  end
+
+end
--- app/models/province.rb
+++ app/models/province.rb
@@ -3,4 +3,7 @@ class Province < ActiveRecord::Base
   validates :name, presence: true
 
   has_many :cities
+
+  include Featurable
+  featurable :geom, [:name]
 end
