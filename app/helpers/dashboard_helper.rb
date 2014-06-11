e49ec2375afc7d0826d14d5325635830c20ac683
Andy Wang <climber2002@gmail.com>
2014-06-11 23:33:45 +0930

    hide other areas and only display gabon

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,2 +1,13 @@
 module DashboardHelper
+  # Generates a google map array which contains the points that wrap
+  # this polygon
+  def polygon_to_gmap_points_array polygon
+    result = "["
+
+    polygon.exterior_ring.points.each do |point|
+      result << "new google.maps.LatLng(#{point.y}, #{point.x}),\n"
+    end
+    result.slice! result.rindex(",\n"), ",\n".size
+    result << "]";
+  end
 end
