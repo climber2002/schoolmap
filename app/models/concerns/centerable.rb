b61104191b215915858235afdd199863a0f31ca2
Andy Wang <climber2002@gmail.com>
2014-06-17 23:00:34 +0930

    add center and zoom when click

--- app/models/concerns/centerable.rb
+++ app/models/concerns/centerable.rb
@@ -6,6 +6,10 @@ module Centerable
       define_method :center do
         read_attribute(geom_attr_name).centroid.to_s
       end
+
+      define_method :center_point do
+        read_attribute(geom_attr_name).centroid
+      end
     end
   end
 end
