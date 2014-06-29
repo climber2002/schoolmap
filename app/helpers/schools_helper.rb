5cedb6ca8467d4fa02d9121eaad160a521e7bfc1
Andy Wang <climber2002@gmail.com>
2014-06-29 13:42:22 +0930

    add markers for grades

--- app/helpers/schools_helper.rb
+++ app/helpers/schools_helper.rb
@@ -1,2 +1,29 @@
 module SchoolsHelper
+
+
+
+  # returns the grade image marker for a school
+  # the format of the image will be markermed_#{color}_#{char}.png
+  def grade_marker_for_school school
+    case school.grade.name.downcase
+    when "pmaternelle"
+      char = 'P'
+      color = "blue"
+    when "primaire"
+      char = "R"
+      color = "orange"
+    when "college"
+      char = "C"
+      color = "red"
+    when "lycee"
+      char = "L"
+      color = "yellow"
+    else
+      char = "U"
+      color = "black"
+    end
+
+    image_path("markers/markermed_#{color}_#{char}.png")
+  end
+
 end
