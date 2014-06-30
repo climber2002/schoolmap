87a078ad6724054b9bd1842b24967b80b1f02b49
Andy Wang <climber2002@gmail.com>
2014-06-30 22:52:30 +0930

    add_bar_chart

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -19,6 +19,8 @@ class SchoolsController < ApplicationController
     @schools = @schools.in_province(params[:province_id]) if params[:province_id]
     @schools = @schools.in_city(params[:city_id]) if params[:city_id]
 
+    set_school_stats
+    
     respond_to do |format|
       format.js
     end
@@ -27,9 +29,28 @@ class SchoolsController < ApplicationController
   private 
 
   def school_params
-    puts "params: #{params}"
     params.require(:school).permit(:name, :address, :zipcode, :director, :website, :phone_number,
       :email, :geom, :city, :city_id, :city_name, :grade_id, :grade_name, :category_id, :category_name, 
       :capacity)
+  end
+
+  def set_school_stats
+    # stats is a hash of hash, for the outer hash the key is the category name, and each category has a hash, 
+    # for the inner hash, the key is the grade and the value is the count
+    @school_count_stats = Hash.new do |hash, missing_key|
+                            hash[missing_key] = Hash.new do |hash, missing_key|
+                                                  hash[missing_key] = 0
+                                                end
+                          end
+    @capacity_stats = Hash.new do |hash, missing_key|
+                            hash[missing_key] = Hash.new do |hash, missing_key|
+                                                  hash[missing_key] = 0
+                                                end
+                        end
+
+    @schools.each do |school|
+      @school_count_stats[school.category.name][school.grade.name] +=1
+      @capacity_stats[school.category.name][school.grade.name] += school.capacity
+    end
   end
 end
